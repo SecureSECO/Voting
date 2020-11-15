bf5b41a8c56f312a1a39b124c2d368c5ee459186
ElenaBaninemeh <elena.baninemeh@yahoo.com>
2020-11-15 18:33:16 +0330

    Updates

---
+++ precache-manifest.820d299caf34e48dc870999a61b94485.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935e2a3822f0e8dd9e6c13188616d6d9",
+    "url": "/Voting/index.html"
+  },
+  {
+    "revision": "bc1ba21c0d3968ec60f7",
+    "url": "/Voting/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "945f3bb569f94f7318ee",
+    "url": "/Voting/static/css/main.7581eb3a.chunk.css"
+  },
+  {
+    "revision": "bc1ba21c0d3968ec60f7",
+    "url": "/Voting/static/js/2.1b86cda6.chunk.js"
+  },
+  {
+    "revision": "4be4126eca2c6dd6ccb4d17c1ab10ab2",
+    "url": "/Voting/static/js/2.1b86cda6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945f3bb569f94f7318ee",
+    "url": "/Voting/static/js/main.cc420abe.chunk.js"
+  },
+  {
+    "revision": "fde388f99ab2b59137c6",
+    "url": "/Voting/static/js/runtime-main.97bf23ab.js"
+  },
+  {
+    "revision": "856ac9d0115fa9abbc7581e22a53994b",
+    "url": "/Voting/static/media/bg2.856ac9d0.jpg"
+  },
+  {
+    "revision": "06ab33ae3e2cbae0f509cd05f2ee6793",
+    "url": "/Voting/static/media/boxicons.06ab33ae.eot"
+  },
+  {
+    "revision": "5bbebd73a37de0cc242e872b4483269e",
+    "url": "/Voting/static/media/boxicons.5bbebd73.woff"
+  },
+  {
+    "revision": "5e99edcb80abc73c7a06aacfddca51bf",
+    "url": "/Voting/static/media/boxicons.5e99edcb.svg"
+  },
+  {
+    "revision": "72589e162de78952d4d6b5ce8b832175",
+    "url": "/Voting/static/media/boxicons.72589e16.ttf"
+  },
+  {
+    "revision": "aeaf4ee6e2c712532f982c8a4dd68b16",
+    "url": "/Voting/static/media/boxicons.aeaf4ee6.woff2"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/Voting/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/Voting/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Voting/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b0d6fa0093865cad7c48d1c8ff6ccbed",
+    "url": "/Voting/static/media/mylogo.b0d6fa00.jpg"
+  },
+  {
+    "revision": "0f04b257c666e13481655674eb6ad85d",
+    "url": "/Voting/static/media/uulogo.0f04b257.png"
+  }
+]);
